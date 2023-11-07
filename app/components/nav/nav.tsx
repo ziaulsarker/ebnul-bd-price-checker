db184953bb432ecac267e477e22f2c77a2e7a41a
ebnulAhad <142676349+ebnulAhad@users.noreply.github.com>
2023-11-07 19:00:55 +0600

    added nav bar

--- app/components/nav/nav.tsx
+++ app/components/nav/nav.tsx
@@ -1,6 +1,26 @@
-function Nav(prop){
+"use client"
+import "./nav.css"
+function Nav(){
+
     return (
-        <nav>  <div> this is navigation</div> </nav>
+        <nav className="nav">
+            <a href="#" className="hover">
+                Discover
+                </a>
+                    <a href="#" className="hover">
+                        Travelling
+                            </a>
+                                <a href="#" className="hover">
+                                    Electronics
+                                            </a>
+                                            <a href="#" className="hover">
+                                                Clothing
+                                                    </a>
+                                                        <a href="#" className="hover">
+                                                                Support
+                                                                    </a>
+                                                                        </nav>
+        
     )
 }
 export default Nav
