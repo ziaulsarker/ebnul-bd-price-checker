dab96ed6f050e4b52e309227a7c4acab4463d427
ebnulAhad <142676349+ebnulAhad@users.noreply.github.com>
2024-02-24 23:44:02 +0600

    recent updates

--- app/components/hero/hero.tsx
+++ app/components/hero/hero.tsx
@@ -3,10 +3,16 @@ import "./hero.css";
 
 function Hero() {
     return (
-        <div className="container ele">
+        <div className="container">
+        <div>
             <h1>Acquire</h1>
             <div className="stext">  the best prices in seconds—never miss a deal</div>
+        </div >
+            <div className="pos">   
+                <input className='searchBox' placeholder="Try searching"/>
+            </div>
         </div>
+        
         
     )
 }
